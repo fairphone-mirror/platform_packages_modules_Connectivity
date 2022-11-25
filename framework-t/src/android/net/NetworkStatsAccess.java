LineageOS/android_packages_modules_Connectivity

commit 4f84ff37091d05733eceb7867bdfb619aca65052

[MS26.1] Remove Process.NETWORK_STACK_UID dependency
Check NetworkStack permission instead.

Test: NetworkStatsAccessTest
Bug: 204830222
Change-Id: I052a1422d145af533b036b0f017ace485b67cbfe

framework-t/src/android/net/NetworkStatsAccess.java
@@ -17,6 +17,7 @@
 package android.net;
 
 import static android.Manifest.permission.READ_NETWORK_USAGE_HISTORY;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.net.NetworkStats.UID_ALL;
 import static android.net.TrafficStats.UID_REMOVED;
 import static android.net.TrafficStats.UID_TETHERING;
@@ -106,7 +107,7 @@ public final class NetworkStatsAccess {
 
     /** Returns the {@link NetworkStatsAccess.Level} for the given caller. */
     public static @NetworkStatsAccess.Level int checkAccessLevel(
-            Context context, int callingUid, String callingPackage) {
+            Context context, int callingPid, int callingUid, String callingPackage) {
         final DevicePolicyManager mDpm = context.getSystemService(DevicePolicyManager.class);
         final TelephonyManager tm = (TelephonyManager)
                 context.getSystemService(Context.TELEPHONY_SERVICE);
@@ -123,8 +124,12 @@ public final class NetworkStatsAccess {
         final boolean isDeviceOwner = mDpm != null && mDpm.isDeviceOwnerApp(callingPackage);
         final int appId = UserHandle.getAppId(callingUid);
 
+        final boolean isNetworkStack = context.checkPermission(
+                android.Manifest.permission.NETWORK_STACK, callingPid, callingUid)
+                == PERMISSION_GRANTED;
+
         if (hasCarrierPrivileges || isDeviceOwner
-                || appId == Process.SYSTEM_UID || appId == Process.NETWORK_STACK_UID) {
+                || appId == Process.SYSTEM_UID || isNetworkStack) {
             // Carrier-privileged apps and device owners, and the system (including the
             // network stack) can access data usage for all apps on the device.
             return NetworkStatsAccess.Level.DEVICE;

service-t/src/com/android/server/net/NetworkStatsService.java
@@ -811,7 +811,7 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
 
     private @NetworkStatsAccess.Level int checkAccessLevel(String callingPackage) {
         return NetworkStatsAccess.checkAccessLevel(
-                mContext, Binder.getCallingUid(), callingPackage);
+                mContext, Binder.getCallingPid(), Binder.getCallingUid(), callingPackage);
     }
 
     /**

fairphone-mirror/platform_packages_modules_Connectivity

commit 8e0e1b6171475de5505aa6dceb39f48a8152b003

Grant Mainline NetworkStack access to network stats
Parts of the network stack moved into a Mainline module, specifically
NetworkStackGoogle on GMS devices. NetworkStatsAccess should allow the
whole network stack to query stats across apps, so check for
PERMISSION_MAINLINE_NETWORK_STACK as well.

Issue: FP3-A13#32
Fixes: 4f84ff3709 ("[MS26.1] Remove Process.NETWORK_STACK_UID dependency")
Test: run gts -m GtsNetworkStackHostTestCases -t com.google.android.gts.networkstack.NetworkStatsHostTest#testNetworkStatsProvider
Change-Id: Ie9d7120903956ae24a31cf7da66a7c89d8cd85c5

framework-t/src/android/net/NetworkStatsAccess.java
@@ -127,9 +127,12 @@ public final class NetworkStatsAccess {
         final boolean isNetworkStack = context.checkPermission(
                 android.Manifest.permission.NETWORK_STACK, callingPid, callingUid)
                 == PERMISSION_GRANTED;
+        final boolean isMainlineNetworkStack = context.checkPermission(
+                NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK, callingPid, callingUid)
+                == PERMISSION_GRANTED;
 
         if (hasCarrierPrivileges || isDeviceOwner
-                || appId == Process.SYSTEM_UID || isNetworkStack) {
+                || appId == Process.SYSTEM_UID || isNetworkStack || isMainlineNetworkStack) {
             // Carrier-privileged apps and device owners, and the system (including the
             // network stack) can access data usage for all apps on the device.
             return NetworkStatsAccess.Level.DEVICE;